0cc64b87abaa6a6f75039bbf0e7a088a1fc3f0fa
doltonsedward <edwarddoltons@gmail.com>
2025-12-07 22:04:06 +0700

    feat: init Footer

---
+++ src/components/section/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BrandingSection from './BrandingSection';
+
+const Footer = () => {
+  return (
+    <div className="bg-white py-[60px] md:py-20">
+      <BrandingSection />
+    </div>
+  );
+};
+
+export default Footer;
